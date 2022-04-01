8d9695c8a37ecc121e26f2adbb26a789e9b5e9da
doyeon <kimsk3113@naver.com>
2022-04-01 19:22:57 +0900

    초기 설정 test코드 삭제

--- src/main/java/com/project/together/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.together.repository;
-
-import com.project.together.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// 테스트
-public interface TestRepository extends JpaRepository <Test, String>{
-
-}
