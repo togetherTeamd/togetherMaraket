15ef554aadcc6dbaed029a8e7d324c5ddab3ba35
Leehyoju97 <dlgywn2420@gmail.com>
2022-04-09 14:31:00 +0900

    ㅇㅇ

--- src/main/java/com/project/together/controller/KakaoLoginController.java
+++ src/main/java/com/project/together/controller/KakaoLoginController.java
@@ -32,7 +32,7 @@ public class KakaoLoginController {
 
     /***
      *
-     * @param code : 인가코드로 access 토큰 받기 받기
+     * @param code : 인가코드로 access 토큰 받기 받기 자기
      * @param
      * @return
      * @throws Exception
