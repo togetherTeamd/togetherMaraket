029e7537de01a261427e5cda11c010d052397379
minroco <minroco@naver.com>
2022-05-18 21:47:34 +0900

    Map 구현 완료, Chat DB 설정 중

---
+++ src/main/java/com/project/together/service/RoomService.java
@@ -0,0 +1,29 @@
+package com.project.together.service;
+
+import com.project.together.entity.Room;
+import com.project.together.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public void save(Room room) {
+        roomRepository.save(room);
+    }
+
+    public Room findOne(String id) {
+        return roomRepository.findOne(id);
+    }
+
+    public Room findById(Long id) { return roomRepository.findById(id); }
+
+    public List<Room> findAll() {return roomRepository.findAll();}
+}
